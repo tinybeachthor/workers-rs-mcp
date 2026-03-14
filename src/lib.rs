d018bce3a56d2111d22681ce3ff22cafb162503b
Martin <25009432+tinybeachthor@users.noreply.github.com>
2026-03-14 12:46:15 -0500

    fix tools file path

--- src/lib.rs
+++ src/lib.rs
@@ -37,8 +37,8 @@ async fn fetch(
     _ctx: worker::Context,
 ) -> worker::Result<Response<Body>> {
     let mut mcp_router = Server::builder()
-        .tools_file("")
         .server_info(env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"))
+        .tools_file("mcp/tools.json")
         .build();
 
     mcp_router.handle_tool("echo", Arc::new(EchoHandler));
